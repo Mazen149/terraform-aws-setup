2b764405b6bf3962b0cde10c9e234df6d0acaf5f
Mazen149 <mazen.mohamed1393@gmail.com>
2025-04-19 11:18:02 +0200

    Added 2 servers with java

--- main.tf
+++ main.tf
@@ -1,18 +1,30 @@
+# module "jenkins_server" {
+#   source = "./modules/server_module"
+#   server_name = "jenkins"
+#   script_path = "install_java.sh"
+#   web_port = 8080
+# }
 
 
-module "jenkins_server" {
+# module "prometheus_server" {
+#   source = "./modules/server_module"
+#   server_name = "prometheus"
+#   script_path = "install_docker_prometheus.sh"
+#   web_port = 9090
+#   is_file_copied = true
+#   file_name = "prometheus.yml"
+# }
+
+module "server1" {
   source = "./modules/server_module"
-  server_name = "jenkins"
-  script_path = "install_java.sh"
+  server_name = "server1"
+  script_path = "install_openjdk.sh"
   web_port = 8080
 }
 
-
-module "prometheus_server" {
+module "server2" {
   source = "./modules/server_module"
-  server_name = "prometheus"
-  script_path = "install_docker_prometheus.sh"
-  web_port = 9090
-  is_file_copied = true
-  file_name = "prometheus.yml"
+  server_name = "server2"
+  script_path = "install_openjdk.sh"
+  web_port = 8081
 }
